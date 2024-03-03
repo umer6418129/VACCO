090988570b3058af3b8e3f750ec7cbcc627ab869
umer <md.farooq.ansari8959149@gmail.com>
2024-03-03 18:45:40 +0500

    work going on new layout for v-2.0

--- database/migrations/Migrate.php
+++ database/migrations/Migrate.php
@@ -1,12 +1,9 @@
 <?php
-require_once '../config/config.php';
-require_once '../config/connection.php';
+
+require_once '../config/DbConnect.php';
 require_once 'BaseMigration.php';
 require_once 'RoleMigration.php';
 require_once 'UserMigration.php';
-
-$conn = connectToDatabase($config);
-
 
 $roleMigration = RoleMigration::up($conn);
 $userMigration = UserMigration::up($conn);
